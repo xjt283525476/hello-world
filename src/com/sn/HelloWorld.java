0d0254479170b590036bbbd8a2ba90ab1e439b91
xuejiangtao <xjt283525476@163.com>
2017-12-06 17:21:28 +0800

    Initial commit

---
+++ src/com/sn/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.sn;
+
+/**
+ * Created by Administrator on 2017/12/6.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
